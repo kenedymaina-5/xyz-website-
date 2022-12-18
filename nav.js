82ebbb7e3de42d20dc0a5a44eb0a161dcfea54df
kenedymaina-5 <76390492+kenedymaina-5@users.noreply.github.com>
2022-12-18 02:11:55 -0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (clear
+    
+    <div>
+      <div id="navbar">
+        <a href="#home">Home</a>
+        <a href="#services">Services</a>
+        <a href="#about">About</a>
+        <a href="#contacts">Contacts</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
